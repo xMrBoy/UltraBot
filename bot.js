d3e70dd39deaf9ead4ae6fefc23c7646d99c75f5
xMrBoy <39704371+xMrBoy@users.noreply.github.com>
2018-05-28 19:20:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = ':'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+  if (msg.content === 'slm') {
+    msg.reply('slm!');
   }
 });
+
 
 client.login('NDUwNjc0OTU0OTQyNTQ1OTIx.De2rWQ.VRSMEmaTZlt6g9JPDvondRe1fnU');
